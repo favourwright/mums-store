db75bfb60cefc65f09f2bf2357a2d734ab32f2f8
Favour <gismozfexie@yahoo.com>
2022-06-22 02:48:10 +0100

    feat: init with main dependencies installed

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    buildModules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+    ],
+    css: [
+        '@/assets/css/main.css',
+    ],
+})
